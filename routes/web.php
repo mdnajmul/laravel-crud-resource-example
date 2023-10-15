0b717c7405639c76d49b49a1b274da019a3997a8
mdnajmul <mdnajmulovi@gmail.com>
2023-10-15 22:15:44 +0600

    redirect root route to products.index route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\ProductController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('products.index');
 });
 
 Route::resource('products',ProductController::class);
